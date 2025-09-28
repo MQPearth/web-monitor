550ac6dadedb26a3720a05b35eb45ab0f7153399
MQPearth <zzxjava@163.com>
2025-09-28 16:36:01 +0800

    企业微信通知

--- README.md
+++ README.md
@@ -8,34 +8,38 @@
 
 3. 网站响应内容不包含指定的字符
 
-若出现异常会通过阿里云短信进行通知
+若出现异常会通过阿里云短信或企业微信进行通知
 
 配置如下
 
 ```json
 {
-  //阿里短信配置
+  // 通知类型 1:阿里云短信 2:企业微信机器人
+  "NotifyType": 1,
+  // 企业微信机器人通知key
+  "WeWorkKey": "xxxxx",
+  // 阿里短信配置
   "EndPoint": "http://dysmsapi.aliyuncs.com",
-  //阿里短信配置
+  // 阿里短信配置
   "AccessKeyId": "*******",
-  //阿里短信配置
+  // 阿里短信配置
   "AccessKeySecret": "*******",
-  //阿里短信配置
+  // 阿里短信配置
   "SignName": "*******",
-  //阿里短信配置
+  // 阿里短信配置
   "TemplateCode": "*******",
-  //阿里短信配置
+  // 阿里短信配置
   "RegionId": "cn-shenzhen",
-  //通知目标
+  // 通知目标
   "PhoneNumbers": "*******",
-  //关键字
+  // 关键字
   "HackedWords": [
     "ransom",
     "money"
   ],
-  //连接超时时间 示例: 300ms
+  // 连接超时时间 示例: 300ms
   "ConnectionTimeout": 300000,
-  //内容读取时间 示例: 300ms
+  // 内容读取时间 示例: 300ms
   "ReadTimeout": 300000
 }
 ```
--- main.cc
+++ main.cc
@@ -19,7 +19,8 @@
 #define HELP_INFO "Invalid argument. Demo: \nweb-monitor -u https://www.baidu.com"
 
 
-std::string get_now_time_str() {
+std::string get_now_time_str()
+{
     auto now = std::chrono::system_clock::now();
     auto ticks = std::chrono::system_clock::to_time_t(now);
     auto local_time = localtime(&ticks);
@@ -28,7 +29,8 @@ std::string get_now_time_str() {
     return time_ss.str();
 }
 
-std::string get_now_time_str_for_sms() {
+std::string get_now_time_str_for_sms()
+{
     auto now = std::chrono::system_clock::now();
     auto ticks = std::chrono::system_clock::to_time_t(now);
     auto local_time = gmtime(&ticks);
@@ -38,7 +40,8 @@ std::string get_now_time_str_for_sms() {
 }
 
 
-Poco::JSON::Object::Ptr get_config_json(char *_config_path) {
+Poco::JSON::Object::Ptr get_config_json(char* _config_path)
+{
     std::string cmd_path(_config_path);
     std::stringstream config_path_ss;
     // example: /usr/bin/do -> /usr/bin/
@@ -47,7 +50,8 @@ Poco::JSON::Object::Ptr get_config_json(char *_config_path) {
     auto config_path = config_path_ss.str();
     std::ifstream ifs(config_path, std::ios::in);
 
-    if (!ifs || !ifs.is_open()) {
+    if (!ifs || !ifs.is_open())
+    {
         throw std::invalid_argument("config.json not exist");
     }
 
@@ -55,34 +59,38 @@ Poco::JSON::Object::Ptr get_config_json(char *_config_path) {
     return parser.parse(ifs).extract<Poco::JSON::Object::Ptr>();
 }
 
-httplib::Result request_target_host(const char *url, int connection_timeout, int read_timeout) {
+httplib::Result request_target_host(const char* url, int connection_timeout, int read_timeout)
+{
     httplib::Client cli(url);
     // 100 milliseconds
     cli.set_connection_timeout(0, connection_timeout);
     // 2 seconds
     cli.set_read_timeout(0, read_timeout);
     auto res = cli.Get("/", {
-            {"User-Agent", "web-monitor/1.0"}
-    });
+                           {"User-Agent", "web-monitor/1.1"}
+                       });
 
     return res;
 }
 
 // check http code and content
-bool check_result(httplib::Result *http_result, Poco::JSON::Object::Ptr json) {
-
-    if (!(*http_result)) {
+bool check_result(httplib::Result* http_result, Poco::JSON::Object::Ptr json)
+{
+    if (!(*http_result))
+    {
         std::cout << "Failed to get target url response" << std::endl;
         return false;
     }
 
 
-    if ((*http_result).error() != httplib::Error::Success) {
+    if ((*http_result).error() != httplib::Error::Success)
+    {
         std::cout << "Failed to connect target url" << std::endl;
         return false;
     }
 
-    if ((*http_result)->status != 200) {
+    if ((*http_result)->status != 200)
+    {
         std::cout << "The target url response code is not equal to 200" << std::endl;
         return false;
     }
@@ -90,9 +98,11 @@ bool check_result(httplib::Result *http_result, Poco::JSON::Object::Ptr json) {
     auto hacked_words = json->getArray("HackedWords");
 
     auto content = (*http_result)->body;
-    for (const auto &item: *(hacked_words.get())) {
+    for (const auto& item : *(hacked_words.get()))
+    {
         auto item_str = item.convert<std::string>();
-        if (content.find(item_str) != -1) {
+        if (content.find(item_str) != -1)
+        {
             std::cout << "find hacked words: " << item_str << ", body: " << content << std::endl;
             return false;
         }
@@ -101,8 +111,8 @@ bool check_result(httplib::Result *http_result, Poco::JSON::Object::Ptr json) {
 }
 
 
-std::string encode(std::string str) {
-
+std::string encode(std::string str)
+{
     str = Poco::replace(str, "%3A", "%253A");
     str = Poco::replace(str, "%7E", "~");
     str = Poco::replace(str, "+", "%2B");
@@ -115,13 +125,15 @@ std::string encode(std::string str) {
 }
 
 
-std::string sign(const std::string &security_key, const std::string &str) {
+std::string sign(const std::string& security_key, const std::string& str)
+{
     Poco::HMACEngine<Poco::SHA1Engine> hmac(security_key);
     hmac.update(str);
     auto digest = hmac.digest();
     std::stringstream ss;
     Poco::Base64Encoder encoder(ss);
-    for (const auto &item: digest) {
+    for (const auto& item : digest)
+    {
         encoder << item;
     }
     encoder.close();
@@ -129,23 +141,27 @@ std::string sign(const std::string &security_key, const std::string &str) {
 }
 
 // Build url and encode key/value
-std::string build_url_params(std::map<std::string, std::string> *map) {
+std::string build_url_params(std::map<std::string, std::string>* map)
+{
     std::stringstream ss;
-    for (const auto &item: *map) {
+    for (const auto& item : *map)
+    {
         ss << encode(item.first)
-           << "="
-           << encode(item.second)
-           << "&";
+            << "="
+            << encode(item.second)
+            << "&";
     }
     auto str = ss.str();
-    if (str.length() > 0) {
+    if (str.length() > 0)
+    {
         str = str.substr(0, str.length() - 1);
     }
     return str;
 }
 
 
-std::string build_send_sms_url(Poco::JSON::Object::Ptr json) {
+std::string build_send_sms_url(Poco::JSON::Object::Ptr json)
+{
     Poco::UUIDGenerator generator;
     std::map<std::string, std::string> kv;
     // ordered parameters
@@ -178,23 +194,29 @@ std::string build_send_sms_url(Poco::JSON::Object::Ptr json) {
 }
 
 
-void check_program_param(int argc, char **argv) {
-    if (argc <= 2 || argc > 3) {
+void check_program_param(int argc, char** argv)
+{
+    if (argc <= 2 || argc > 3)
+    {
         throw std::invalid_argument(HELP_INFO);
     }
 
-    if (argc == 3) {
-        if (strcmp(argv[1], "-u") != 0) {
+    if (argc == 3)
+    {
+        if (strcmp(argv[1], "-u") != 0)
+        {
             throw std::invalid_argument(HELP_INFO);
         }
     }
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv)
+{
     std::cout << "-----------------------" << std::endl;
     std::cout << get_now_time_str() << ": run web-monitor." << std::endl;
 
-    try {
+    try
+    {
         check_program_param(argc, argv);
 
         auto json = get_config_json(argv[0]);
@@ -205,20 +227,57 @@ int main(int argc, char **argv) {
                                           json->get("ReadTimeout"));
 
         // if hacked, then send sms
-        if (!check_result(&result, json)) {
+        if (!check_result(&result, json))
+        {
+            int notify_type = json->get("NotifyType").convert<int>();
 
-            std::string url = build_send_sms_url(json);
-            httplib::Client cli(json->get("EndPoint").convert<std::string>());
-            auto res = cli.Get(url);
+            if (notify_type == 1)
+            {
+                std::string url = build_send_sms_url(json);
+                httplib::Client cli(json->get("EndPoint").convert<std::string>());
+                auto res = cli.Get(url);
+                if (res->status == 200)
+                {
+                    std::cout << "send success: " << res->status << std::endl;
+                }
+                else
+                {
+                    std::cout << "send fail, code:  " << res->status << ", body: " << res->body << std::endl;
+                }
+            }
+            else if (notify_type == 2)
+            {
+                auto we_work_key = json->get("WeWorkKey").convert<std::string>();
+                httplib::Client cli("https://qyapi.weixin.qq.com");
+                auto res = cli.Post("/cgi-bin/webhook/send?key=" + we_work_key,
+                                    R"({"msgtype": "text","text": {"content":"目标网站出现异常， 请及时处理"}})",
+                                    "application/json; charset=utf-8");
+                if (res->status == 200)
+                {
+                    auto body = res->body;
+                    Poco::JSON::Parser parser;
+                    auto body_json = parser.parse(body).extract<Poco::JSON::Object::Ptr>();
 
-            if (res->status == 200) {
-                std::cout << "send success: " << res->status << std::endl;
-            } else {
-                std::cout << "send fail, code:  " << res->status << ", body: " << res->body << std::endl;
+                    auto errcode = body_json->get("errcode").convert<int>();
+
+                    if (errcode == 0)
+                    {
+                        std::cout << "send success: " << res->status << std::endl;
+                    }
+                    else
+                    {
+                        std::cout << "send fail, body: " << res->body << std::endl;
+                    }
+                }
+                else
+                {
+                    std::cout << "send fail, code:  " << res->status << ", body: " << res->body << std::endl;
+                }
             }
         }
-
-    } catch (std::exception &e) {
+    }
+    catch (std::exception& e)
+    {
         std::cout << e.what() << std::endl;
     }
     std::cout << "web-monitor end" << std::endl;
